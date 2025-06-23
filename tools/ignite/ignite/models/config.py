108a2571a1b94941d60e82dfc36183580386a8bb
midoriiro <alexis.bekhdadi@gmail.com>
2025-06-23 21:45:00 +0000

    feat(ignite): add config models

---
+++ tools/ignite/ignite/models/config.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, Field
+from ignite.models.container import Container
+from ignite.models.workspace import Workspace
+
+
+class Configuration(BaseModel):
+    """
+    Represents a configuration.
+    """
+
+    container: Container = Field(..., description="Container configuration")
+    workspace: Workspace = Field(..., description="Workspace configuration")
+
+Configuration.model_rebuild()
